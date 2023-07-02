beab946ef86fb41b1d7847d76f90262586ebe85f
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2023-07-01 21:34:05 -0300

    :boom: update layout

--- src/components/DivNumbers.tsx
+++ src/components/DivNumbers.tsx
@@ -1,6 +1,6 @@
 export function DivNumbers() {
   return (
-    <div className="absolute z-50 -mt-16 flex h-[13.125rem] w-full items-center justify-between rounded-md bg-primary-brand-beige px-[9rem] mobile:h-auto mobile:flex-col mobile:gap-14 mobile:py-10">
+    <div className="absolute z-40 -mt-16 flex h-[13.125rem] w-full items-center justify-between rounded-md bg-primary-brand-beige px-[9rem] mobile:h-auto mobile:flex-col mobile:gap-14 mobile:py-10">
       <div className="mr-[6.25rem] flex flex-col items-center border-r border-primary-brand-green pr-[6.25rem] mobile:mr-0 mobile:border-none mobile:pr-0">
         <span className="text-48 font-bold text-secundary-gray-headline">
           +3500
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,26 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Links } from './Links'
 import { AlignRight, X } from 'lucide-react'
 import Link from 'next/link'
 import logo from '../app/assets/logo.svg'
-import logoMobile from '../app/assets/logo-footer.svg'
+import logoWhite from '../app/assets/logo-footer.svg'
 import Image from 'next/image'
 
 export function Header() {
   const [openMenuMobile, setOpenMenuMobile] = useState(false)
+  const [isSticky, setSticky] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setSticky(true)
+      } else {
+        setSticky(false)
+      }
+    })
+  }, [])
 
   function handleOpenMenuMobile() {
     setOpenMenuMobile(!openMenuMobile)
@@ -21,10 +32,16 @@ export function Header() {
   }
 
   return (
-    <header className={'relative  bg-primary-brand-green-light py-[1.625rem]'}>
+    <header
+      className={`${
+        isSticky
+          ? 'fixed left-0 top-0 z-50 w-full bg-primary-brand-green py-[1.625rem]'
+          : 'relative bg-primary-brand-green-light py-[1.625rem]'
+      }`}
+    >
       <nav
         className={
-          'mx-auto flex w-[90%] max-w-[70rem]  items-center justify-between'
+          ' mx-auto flex w-[90%] max-w-[70rem] items-center justify-between'
         }
       >
         <div
@@ -34,7 +51,9 @@ export function Header() {
         >
           <span>
             {openMenuMobile === true ? (
-              <Image src={logoMobile} width={132} height={17} alt="" />
+              <Image src={logoWhite} width={132} height={17} alt="" />
+            ) : isSticky === true ? (
+              <Image src={logoWhite} width={132} height={17} alt="" />
             ) : (
               <Image src={logo} width={132} height={17} alt="" />
             )}
@@ -52,6 +71,8 @@ export function Header() {
             className={`${
               openMenuMobile === true
                 ? 'text-20 font-bold text-primary-brand-green-light'
+                : isSticky === true
+                ? 'text-primary-brand-green-light'
                 : "relative text-16 text-primary-brand-green before:absolute before:left-0 before:top-0 before:mt-8 before:h-[2px] before:w-0 before:rounded-full before:bg-primary-brand-green before:content-[''] hover:font-bold hover:before:w-full hover:before:transition-all"
             }`}
             onClick={handleCloseMenuMobile}
@@ -64,6 +85,8 @@ export function Header() {
             className={`${
               openMenuMobile === true
                 ? 'text-20 font-bold text-primary-brand-green-light'
+                : isSticky === true
+                ? 'text-primary-brand-green-light'
                 : "relative text-16 text-primary-brand-green before:absolute before:left-0 before:top-0 before:mt-8 before:h-[2px] before:w-0 before:rounded-full before:bg-primary-brand-green before:content-[''] hover:font-bold hover:before:w-full hover:before:transition-all"
             }`}
             onClick={handleCloseMenuMobile}
@@ -76,6 +99,8 @@ export function Header() {
             className={`${
               openMenuMobile === true
                 ? 'text-20 font-bold text-primary-brand-green-light'
+                : isSticky === true
+                ? 'text-primary-brand-green-light'
                 : "relative text-16 text-primary-brand-green before:absolute before:left-0 before:top-0 before:mt-8 before:h-[2px] before:w-0 before:rounded-full before:bg-primary-brand-green before:content-[''] hover:font-bold hover:before:w-full hover:before:transition-all"
             }`}
             onClick={handleCloseMenuMobile}
@@ -88,6 +113,8 @@ export function Header() {
             className={`${
               openMenuMobile === true
                 ? 'text-20 font-bold text-primary-brand-green-light'
+                : isSticky === true
+                ? 'text-primary-brand-green-light'
                 : "relative text-16 text-primary-brand-green before:absolute before:left-0 before:top-0 before:mt-8 before:h-[2px] before:w-0 before:rounded-full before:bg-primary-brand-green before:content-[''] hover:font-bold hover:before:w-full hover:before:transition-all"
             }`}
             onClick={handleCloseMenuMobile}
@@ -112,7 +139,11 @@ export function Header() {
         </div>
 
         <a
-          className="flex w-[13rem] items-center justify-center rounded-3xl border border-primary-brand-green px-[1.5rem] py-[0.625rem] text-14 font-bold uppercase text-primary-brand-green transition-all hover:bg-primary-brand-green hover:text-primary-brand-green-light mobile:hidden"
+          className={`${
+            isSticky === true
+              ? 'flex w-[13rem] items-center justify-center rounded-3xl border border-primary-brand-green-light px-[1.5rem] py-[0.625rem] text-14 font-bold uppercase text-primary-brand-green-light transition-all hover:bg-primary-brand-green-light hover:text-primary-brand-green mobile:hidden'
+              : 'flex w-[13rem] items-center justify-center rounded-3xl border border-primary-brand-green px-[1.5rem] py-[0.625rem] text-14 font-bold uppercase text-primary-brand-green transition-all hover:bg-primary-brand-green hover:text-primary-brand-green-light mobile:hidden'
+          } `}
           href="#"
         >
           Agendar consulta
@@ -128,6 +159,8 @@ export function Header() {
         >
           {openMenuMobile === true ? (
             <X size={40} className="text-primary-brand-green-light" />
+          ) : isSticky === true ? (
+            <AlignRight size={40} className="text-primary-brand-green-light" />
           ) : (
             <AlignRight size={40} className="text-primary-brand-green" />
           )}
